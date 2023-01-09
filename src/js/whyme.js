c217333494ad167ce0e05f8f7368f2abae2fd99d
valyukhov <alexzed@bk.ru>
2023-01-09 08:30:57 +0300

    change name of faq

--- src/js/whyme.js
+++ src/js/whyme.js
@@ -1,21 +1,21 @@
-const faqWrapper = document.querySelector(".q-wrapper");
-const faqHeaders = faqWrapper.querySelectorAll(".q-header");
-const faqContentElements = faqWrapper.querySelectorAll(".q-content");
-const faqImages = faqWrapper.querySelectorAll(".q-image");
+const whymeWrapper = document.querySelector(".q-wrapper");
+const whymeHeaders = whymeWrapper.querySelectorAll(".q-header");
+const whymeContentElements = whymeWrapper.querySelectorAll(".q-content");
+const whymeImages = whymeWrapper.querySelectorAll(".q-image");
 
 function openCoverElement() {
  
-  faqContentElements[this.dataset.faq-1].classList.toggle('max-h-0');
+  whymeContentElements[this.dataset.whyme-1].classList.toggle('max-h-0');
   
-  faqContentElements[this.dataset.faq-1].classList.toggle('max-h-[500px]');
-  faqImages[this.dataset.faq-1].classList.toggle('rotate-0');
-  faqImages[this.dataset.faq-1].classList.toggle('rotate-180');
+  whymeContentElements[this.dataset.whyme-1].classList.toggle('max-h-[500px]');
+  whymeImages[this.dataset.whyme-1].classList.toggle('rotate-0');
+  whymeImages[this.dataset.whyme-1].classList.toggle('rotate-180');
   
 }
 
 
 
 
-faqHeaders.forEach((e) =>{
+whymeHeaders.forEach((e) =>{
     e.addEventListener("click", openCoverElement);
   });
